7ac34144f52737be1df140634fb1e663ab1d4781
Michael Benoit <michael@enigma.com>
2018-09-24 17:50:58 -0400

    add websockets and status updates

--- application.py
+++ application.py
@@ -1,11 +1,10 @@
 import os
 import platform
-import time
 import random
 import logging
-import uuid
 
 from flask import Flask, render_template, request, make_response
+from flask_socketio import SocketIO, join_room, emit
 from selenium import webdriver
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.common.by import By
@@ -20,12 +19,21 @@ from PIL import Image, ImageChops
 
 # Setting debug to True enables debug output.
 DEBUG_FLAG = bool(os.getenv("DEBUG"))
-IMAGE_DIR = os.getenv("IMAGE_DIR", 'images')
+VERBOSE_FLAG = bool(os.getenv("VERBOSE", True))
+IMAGE_DIR = os.getenv("IMAGE_DIR", os.path.join(os.getcwd(),'images'))
+INFO_NAMESPACE = '/info'
+LOGGER = logging.getLogger('docsend_scraper')
+logging.basicConfig()
 
 
 # EB looks for an 'application' callable by default.
 application = Flask(__name__)
+application.config['SECRET_KEY'] = 'secret!'
 bootstrap = Bootstrap(application)
+
+socketio = SocketIO(application)
+
+
 def trim(im):
     bg = Image.new(im.mode, im.size, im.getpixel((50,50)))
     diff = ImageChops.difference(im, bg)
@@ -60,13 +68,13 @@ def normalize_url(url_or_id):
 
 def load_first_page(browser, url):
     browser.get(url)
-    time.sleep(2)
+    socketio.sleep(2)
     try:
         element = WebDriverWait(browser, 4).until(
             EC.presence_of_element_located((By.ID, "youtube-modal"))
         )
     except TimeoutException:
-        logging.error("failed")
+        LOGGER.error("failed")
         if browser.title == '404 Page Not Found':
             raise ApplicationError(f'`{browser.current_url}` returned a 404')
         else:
@@ -78,18 +86,18 @@ def send_auth(browser, email, password):
     try:
         email_element = browser.find_element_by_name('visitor[email]')
         email_element.send_keys(email)
-        time.sleep(1)
+        socketio.sleep(1)
     except:
-        logging.debug("no e-mail required")
+        LOGGER.debug("no e-mail required")
         return
     try:
         pass_element = browser.find_element_by_name('visitor[passcode]')
         pass_element.send_keys(password)
     except:
-        logging.debug("no password required")
+        LOGGER.debug("no password required")
     email_element.send_keys(Keys.TAB)
     email_element.send_keys(Keys.ENTER)
-    time.sleep(1)
+    socketio.sleep(1)
 
 
 def load_pages(browser):
@@ -100,7 +108,7 @@ def load_pages(browser):
     while not is_stable:
         browser.switch_to_active_element().send_keys(Keys.RIGHT)
         # not sure why this is random...
-        time.sleep(0.4 + random.randint(1, 100) / 100)
+        socketio.sleep(0.4 + random.randint(1, 100) / 100)
         pages = browser.find_elements_by_css_selector(".preso-view.page-view")
         urls = [page.get_attribute("src") for page in pages]
         if any("blank.gif" in s for s in urls):
@@ -115,9 +123,10 @@ def save_pages(req_id, browser, urls):
     """Save each page as a png and return a list of paths to the saved images"""
     image_paths = []
     for page_num, url in enumerate(urls, 1):
+        socketio.emit("page update", {'page': page_num}, namespace=INFO_NAMESPACE, room=req_id)
         browser.get(url)
         image_path = f"{IMAGE_DIR}/{req_id}_{page_num}.png"
-        time.sleep(1)
+        socketio.sleep(1)
         browser.save_screenshot(image_path)
         im = Image.open(image_path)
         trimmed_im = trim(im)
@@ -127,7 +136,7 @@ def save_pages(req_id, browser, urls):
     return image_paths
 
 
-def generate_pdf(image_paths):
+def generate_pdf(req_id, image_paths):
     """Take the images and create a PDF"""
     if not image_paths:
         raise ApplicationError("No images")
@@ -139,50 +148,89 @@ def generate_pdf(image_paths):
     pdf = FPDF("L", "pt", [wwidth, wheight])
     pdf.set_margins(0, 0, 0)
 
-    for image_path in image_paths:
+    for page_num, image_path in enumerate(image_paths, 1):
+        socketio.emit("page update", {'page': page_num}, namespace=INFO_NAMESPACE, room=req_id)
         pdf.add_page()
-        pdf.image(image_path, 0, 0, w=wwidth)
-        return pdf
+        pdf.image(image_path, 0, 0)
     return pdf
 
 
-@application.route('/savepdf', methods = ['POST'])
-def savepdf(url="", emailad="", emailpass=""):
+def stage_update(req_id, stage):
+    socketio.emit('stage update', {'stage': stage}, namespace=INFO_NAMESPACE, room=req_id)
+    LOGGER.info("stage updated: %s request: %s", stage, str(req_id))
 
-    req_id = uuid.uuid4()
+
+def upload_to_store(pdf, name):
+    pdf.output(name)
+
+
+def run_job(req_id, url, email, password):
+    LOGGER.info("job: %s url: %s email: %s", req_id, url, email)
+    image_paths = []
+    try:
+        url, id_ = normalize_url(url)
+        browser = setup_browser()
+        load_first_page(browser, url)
+        stage_update(req_id, 'setting auth')
+        send_auth(browser, email, password)
+        stage_update(req_id, 'loading slides')
+        urls = load_pages(browser)
+        stage_update(req_id, 'converting to images')
+        image_paths = save_pages(req_id, browser, urls)
+        browser.close()
+        stage_update(req_id, 'creating PDF')
+        pdf = generate_pdf(req_id, image_paths)
+        stage_update(req_id, 'done')
+    finally:
+        for i in image_paths:
+            os.remove(i)
+    return pdf
+
+
+@application.route('/savepdf', methods=['POST'])
+def savepdf():
     url = request.form['url']
     emailad = request.form['emailad'].encode("ascii")
     emailpass = request.form['emailpass'].encode("ascii")
-
-    url, id_ = normalize_url(url)
-    browser = setup_browser()
-    load_first_page(browser, url)
-    send_auth(browser, emailad, emailpass)
-    urls = load_pages(browser)
-    image_paths = save_pages(req_id, browser, urls)
-    pdf = generate_pdf(image_paths)
-
-    for i in (image_paths):
-        os.remove(i)
-
-    cdir = os.getcwd()
-    browser.close()
-
-    # now serve the PDF
-    response = make_response(pdf.output(dest='S'))
+    req_id = request.form['id']
+    id_ = url
+    pdf = None
+    try:
+        url, id_ = normalize_url(url)
+        pdf = run_job(req_id, url, emailad, emailpass)
+    except ApplicationError as err:
+        socketio.emit('error update',
+                      {'detail': err.args[0]},
+                      namespace=INFO_NAMESPACE,
+                      room=req_id)
+    response = make_response(pdf.output(dest='S').encode('latin1'))
     response.headers.set('Content-Disposition', 'attachment', filename=id_ + '.pdf')
     response.headers.set('Content-Type', 'application/pdf')
     return response
 
 
 @application.route('/')
-def serve_front_page():
-    return render_template('index.html')
+def index():
+    return render_template('index.html', async_mode=socketio.async_mode)
+
+
+@socketio.on("join", namespace=INFO_NAMESPACE)
+def join(message):
+    join_room(message['room'])
+    LOGGER.debug('%s joined room %s in namespace %s',
+                 request.sid,
+                 message['room'],
+                 request.namespace)
+
 
 # run the app.
 if __name__ == "__main__":
     application.debug = DEBUG_FLAG
-    if DEBUG_FLAG:
-        logging.root.setLevel(logging.DEBUG)
+    logging.root.setLevel(logging.INFO)
+    if DEBUG_FLAG or VERBOSE_FLAG:
+        LOGGER.setLevel(logging.DEBUG)
+    else:
+        logging.getLogger('engineio').setLevel(logging.WARN)
+        logging.getLogger('socketio').setLevel(logging.WARN)
     os.makedirs(IMAGE_DIR, exist_ok=True)
-    application.run(host='0.0.0.0')
+    socketio.run(application, host='0.0.0.0', debug=DEBUG_FLAG)
